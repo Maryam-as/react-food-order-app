543bec9b81e837a63569e07cfdf126ed55c7e9b0
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-06-13 23:41:47 +0330

    feat: add cartReducer and integrate useReducer in CartContextProvider
    - Defined cartReducer function to handle ADD_ITEM and REMOVE_ITEM actions
    - Integrated useReducer into CartContextProvider for cart state management
    - Prepares context to manage cart operations in a scalable, maintainable way

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 // Set default context value to enable better auto-completion and tooling support
 const CartContext = createContext({
@@ -7,7 +7,21 @@ const CartContext = createContext({
   removeItem: (id) => {},
 });
 
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // TODO: update the state to add a meal item
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    // TODO: remove an item from the state
+  }
+
+  return state;
+}
+
 export function CartContextProvider({ children }) {
+  useReducer();
+
   return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 
