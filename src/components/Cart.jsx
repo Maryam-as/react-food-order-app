6383071d089d5083dd4b9541ec71064ef36d861a
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-06-27 21:36:39 +0330

    feat: control Cart modal visibility using UserProgressContext
    - Accessed UserProgressContext in Cart component
    - Tied Modal `open` prop to context's progress state
    - Enables centralized control over cart modal visibility

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,11 @@ import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.price * item.quantity,
@@ -13,7 +15,7 @@ export default function Cart() {
   );
 
   return (
-    <Modal className="cart">
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
