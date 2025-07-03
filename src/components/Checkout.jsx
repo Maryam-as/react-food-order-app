ea1b16a6e001a40b566356a12bb6a353c6f346c3
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-07-03 12:20:45 +0330

    feat: integrate reusable Input component into Checkout form for user details

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
+import Input from "./UI/Input.jsx";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
@@ -16,7 +17,13 @@ export default function Checkout() {
       <form>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        // TODO: add various input elements
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
       </form>
     </Modal>
   );
