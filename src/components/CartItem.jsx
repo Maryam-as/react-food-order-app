c39a62dfb592570c21931aff7064f65795c51469
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-07-03 10:12:44 +0330

    feat: create basic structure for CartItem component with placeholder content and actions

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,12 @@
+export default function CartItem() {
+  return (
+    <li className="cart-item">
+      <p>NAME - QTY X PRICE</p>
+      <p className="cart-item-actions">
+        <button>-</button>
+        <span>QTY</span>
+        <button>+</button>
+      </p>
+    </li>
+  );
+}
