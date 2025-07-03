35f58dbb52787675b445b14482a94d0ffa4c7cca
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-07-03 21:15:12 +0330

    feat: add handleSubmit handler to process Checkout form submission
    Defined a handleSubmit function to extract form data using FormData and
    prevent default form submission behavior. Set this handler as the
    onSubmit prop of the Checkout form. Prepares the structure for sending
    customer data to the backend.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -19,12 +19,21 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    // TODO: send http request to the backend
+  }
+
   return (
     <Modal
       open={userProgressCtx.progress === "checkout"}
       onClose={handleCloseCheckout}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />
